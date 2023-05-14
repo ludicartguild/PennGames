edd76ba4f00c8d25c72c169dea7146cf8b196ea2
Jonathan Winters <wintersfile@gmail.com>
2023-05-14 14:35:40 -0500

    fontLoader refactored to use map function

--- Pencil_Sort_Refactored/src/fontsLoader.js
+++ Pencil_Sort_Refactored/src/fontsLoader.js
@@ -1,24 +1,19 @@
-
 // Gets font family names from @font-faces in fonts.css and ensures they are loaded
 // @font-faces with base64 must be added to fonts.css
 function fontsLoader(){}
 fontsLoader.init = function(){
     this.debugFonts = false; // If true, console.logs font family names when loaded
 
-    this.fontFamiliesArray = [];
-    // Get all fonts from the styleSheet css rules
-    let fontFaces = document.styleSheets[0].cssRules;
     
-    for(let i=0; i < fontFaces.length; i++){
+    // Get all fonts from the styleSheet css rules
+    let fontFaces = Object.entries(document.styleSheets[0].cssRules);
+    this.fontFamiliesArray = Object.entries(fontFaces).map((fontString) => {
         // convert @font-face object to string
-        let fontString = fontFaces[i].cssText.toString();
         // Get font family name
-        let fontFamily = fontString.split('font-family: ')[1].split(';',1)[0];
         // Remove extra double quotes ""
-        fontFamily = fontFamily.replace(/['"]+/g, '');
-        // Add font family name to an array to be loaded
-        this.fontFamiliesArray.push(fontFamily);  
-    }
+        // Add font family name to an array to be loaded 
+        return fontString[1][1].cssText.toString().split('font-family: ')[1].split(';',1)[0].replace(/['"]+/g, '');
+    });
 }
 
 fontsLoader.create = function(){
